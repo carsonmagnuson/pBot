ef403fe5ec78f2a3b89274a377560f01f014183d
Carson Magnuson <80717302+carsonmagnuson@users.noreply.github.com>
2021-11-30 19:24:42 -0500

    fix build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,15 @@
 plugins {
-    java
+  java
+  application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+application {
+  mainClass.set("cs222.pbot.Main")
 }
